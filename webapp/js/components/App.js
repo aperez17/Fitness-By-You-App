37ee0fbfdfacd87242e7ce5802847131f5135693
Alex Perez <aperrez17@gmail.com>
2018-04-22 18:53:58 -0400

    Handled verfication of authenticity

--- webapp/js/components/App.js
+++ webapp/js/components/App.js
@@ -4,20 +4,40 @@ import _ from 'lodash';
 import IfElse from './Common/IfElse';
 import Login from './Login/Login';
 import Home from './Home/Home';
+import ServiceClient from '../service/ServiceClient';
 
 export default class App extends ComponentWithAlerts {
   constructor(props) {
     super(props);
     this.state = {
-      authenticated: false
+      authenticated: null
     };
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   onLoginSuccess() {
     this.setState({
       authenticated: true
     });
+  }
+
+  componentWillMount() {
+    this.isAuthenticated();
+  }
+
+  isAuthenticated() {
+    if (_.isNil(this.state.authenticated)) {
+        ServiceClient.get("/api/login").then(() => {
+            this.setState({
+                authenticated: true
+            })
+        }).fail((err) => {
+            this.setState({
+                authenticated: false
+            })
+        })
+    }
   }
 
   render() {
